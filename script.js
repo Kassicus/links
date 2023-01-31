6ee04c25294ffc1d2675fc8d36682dbfa23fbefa
Kason Suchow <kason.suchow@gmail.com>
2023-01-31 13:43:09 -0700

    First Mass Push
    Pushing most of the content to github.

---
+++ script.js
@@ -0,0 +1,179 @@
+function connectwise() {
+    window.open('http://support.archnexus.com/', '_blank');
+}
+
+function barracuda() {
+    window.open('https://ess.barracudanetworks.com/', '_blank');
+}
+
+function thycotic(){
+    window.open('https://thycotic.archnexus.com/ss/', '_blank');
+}
+
+function wordpress(){
+    window.open('http://nexus.archnexus.com/wp-admin/index.php', '_blank');
+}
+
+function microsoft(){
+    window.open('https://admin.microsoft.com/Adminportal/Home#/homepage', '_blank');
+}
+
+function mural(){
+    window.open('', '_blank');
+}
+
+function sketchup(){
+    window.open('https://ecom-prd.trimblepaas.com/account/organization/1605558605548987/members', '_blank');
+}
+
+function goodsync(){
+    window.open('https://jobs.goodsync.com/ui/dashboard', '_blank');
+}
+
+function zoom(){
+    window.open('https://zoom.us/account', '_blank');
+}
+
+function canvas(){
+    window.open('https://cvidportal.canvasgfx.com/product/membership-details/1', '_blank');
+}
+
+function citrix(){
+    window.open('https://www.citrix.com/account/', '_blank');
+}
+
+function malwarebytes(){
+    window.open('https://cloud.malwarebytes.com/auth/login', '_blank');
+}
+
+function vmware(){
+    window.open('https://vcenter.archnexus.com/ui/', '_blank');
+}
+
+function duo(){
+    window.open('https://admin-81fe44ab.duosecurity.com/', '_blank');
+}
+
+function autodesk(){
+    window.open('https://manage.autodesk.com/home/', '_blank');
+}
+
+function bim360(){
+    window.open('https://admin.b360.autodesk.com/admin/af39f38a-a13a-4038-9b6d-84ab7618d482/projects/', '_blank');
+}
+
+function clarity(){
+    window.open('http://slcclarity1/CentralAdministrator/Server', '_blank');
+}
+
+function wfhdoc(){
+    window.open('https://docs.google.com/spreadsheets/d/1NaLpn4t9cSRKYmHbvE1pZSj3Z4gus4ufaZrCb5bTsrM/edit#gid=0', '_blank');
+}
+
+function github(){
+    window.open('https://github.com/ArchitecturalNexus/deploy-nexus-toolbar', '_blank');
+}
+
+function maps(){
+    window.open('http://nexus.archnexus.com/office-maps/', '_blank');
+}
+
+function forms(){
+    window.open('http://nexus.archnexus.com/standards/forms-templates/', '_blank');
+}
+
+function s2sac(){
+    window.open('http://10.3.1.61/', '_blank');
+}
+
+function s2slc(){
+    window.open('http://192.168.45.11/', '_blank');
+}
+
+function graywater(){
+    window.open('http://bms.archnexus.com/login', '_blank');
+}
+
+function pelican(){
+    window.open('https://arch-nexus.officeclimatecontrol.net/#_indexPage', '_blank');
+}
+
+function assets(){
+    window.open('http://assets.archnexus.com/menu.php', '_blank');
+}
+
+function sharefile(){
+    window.open('https://archnexus.sharefile.com/dashboard', '_blank');
+}
+
+function vantagepoint(){
+    window.open('https://archnexus.deltekfirst.com/ArchNexus/app/', '_blank');
+}
+
+function nslookup(){
+    window.open('https://www.nslookup.io/', '_blank');
+}
+
+function bluebeam(){
+    window.open('https://org-admin.bluebeam.com', '_blank');
+}
+
+function knowbe4(){
+    window.open('https://training.knowbe4.com/ui/login', '_blank');
+}
+
+function purpleair(){
+    window.open('https://map.purpleair.com/1/mAQI/a10/p604800/cC0#14.07/40.72209/-111.81005', '_blank');
+}
+
+function virtualkeypad(){
+    window.open('https://www.virtualkeypad.com', '_blank');
+}
+
+function emtrain(){
+    window.open('https://archnexus2.ai.emtrain.com/todo/programs/1978/certificate', '_blank');
+}
+
+function hydrawise(){
+    window.open('https://app.hydrawise.com/config/zones');
+}
+
+function buildingevent(){
+    window.open('https://docs.google.com/spreadsheets/d/1Mjfd522fVkulitWGC86crsnhWLwoWRvYrsukB7Ks0YI/edit#gid=0');
+}
+
+function inhabit(){
+    window.open('http://inhabit.archnexus.com/dashboard?site_code=inhabit');
+}
+
+function viewglass(){
+    window.open('https://view.com/account/signin');
+}
+
+function twingate(){
+    window.open('https://archnexus.twingate.com/');
+}
+
+function webmail(){
+    window.open('https://slc.archnexus.com/owa/auth/logon.aspx?replaceCurrent=1&url=https%3a%2f%2fslc.archnexus.com%2fowa%2f');
+}
+
+function entview(){
+    window.open('https://evue.archnexus.com/#floorplan/%2Fgraphicalregion%2F13/view');
+}
+
+function github_personal(){
+    window.open('https://github.com/Kassicus');
+}
+
+function youtube(){
+    window.open('https://www.youtube.com/');
+}
+
+function gmail(){
+    window.open('https://mail.google.com/mail/u/0/#inbox');
+}
+
+function drive(){
+    window.open('https://drive.google.com/drive/my-drive');
+}
